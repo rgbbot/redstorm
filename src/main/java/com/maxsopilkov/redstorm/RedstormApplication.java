c3888865f62a06c42bb6a2e33369cb518743fbb8
Максим Сопилков <C:\Users\pc\AppData\Roaming\The Bat!>
2018-04-09 14:48:08 +0300

    deploy

---
+++ src/main/java/com/maxsopilkov/redstorm/RedstormApplication.java
@@ -0,0 +1,12 @@
+package com.maxsopilkov.redstorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedstormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedstormApplication.class, args);
+	}
+}
---
+++ src/test/java/com/maxsopilkov/redstorm/RedstormApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maxsopilkov.redstorm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedstormApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
